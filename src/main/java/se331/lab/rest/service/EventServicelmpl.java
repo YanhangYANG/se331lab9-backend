12081a31b383f58853ab38330b7272040bcfce90
yanhang_yang <yanhang_y@cmu.ac.th>
2023-09-12 01:06:09 +0700

    6.6

--- src/main/java/se331/lab/rest/service/EventServicelmpl.java
+++ src/main/java/se331/lab/rest/service/EventServicelmpl.java
@@ -3,9 +3,11 @@ package se331.lab.rest.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import se331.lab.rest.dao.EventDao;
+import se331.lab.rest.dao.OrganizerDao;
 import se331.lab.rest.entity.Event;
-
+import se331.lab.rest.entity.Organizer;
 
 
 @Service
@@ -13,6 +15,7 @@ import se331.lab.rest.entity.Event;
 public class EventServicelmpl implements EventService {
 
     final EventDao eventDao;
+    final OrganizerDao organizerDao;
     @Override
     public Integer getEventSize() {
         return eventDao.getEventSize();
@@ -29,7 +32,12 @@ public class EventServicelmpl implements EventService {
     }
 
     @Override
+     @Transactional
     public Event save(Event event) {
+        Organizer organizer =
+                organizerDao.findById(event.getOrganizer().getId()).orElse(null);
+        event.setOrganizer(organizer);
+        organizer.getOwnEvents().add(event);
         return eventDao.save(event);
     }
 
